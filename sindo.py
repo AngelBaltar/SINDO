59898a016ff7f037a650edb9fb1ccd482288fd4f
Angel <angel.baltar.diaz@gmail.com>
2019-11-06 20:38:10 +0100

    update sindo to paralelize analysis

--- sindo.py
+++ sindo.py
@@ -5,6 +5,8 @@ import argparse
 from all_index_nasdaq import *
 import sys
 import math
+from multiprocessing import Pool
+import time
 
 
 def mean(l):
@@ -60,41 +62,43 @@ class StockDaykMeasure(object):
 	def get_grad(self):
 		return get_close()-get_open()
 
-class StockDataAnalysys(object):
+class StockHistoricMeasure(object):
+
 	def __init__(self,date_start,date_end,idx):
 		self._date_start=date_start
 		self._date_end=date_end
-		self._st_datas={}
 		self._idx=idx
-		self._score=0
-		self._analyze()
-	
-	def __str__(self):
-		return "%s [%s]->[%s]:%f"%(self._idx,str(self._date_start),str(self._date_end),self.get_score())	
+		self._st_datas={}
+		self.fill_data()
 
-	def _get_stock_measure(self,date):
+	def __str__(self):
+		return "%s [%s]->[%s]"%(self._idx,str(self._date_start),str(self._date_end))
+
+	def get_start_date(self):
+		return self._date_start
+
+	def get_end_date(self):
+		return self._date_end
+
+	def get_idx(self):
+		return self._idx
+
+	def __iter__(self):
+		return self._st_datas.__iter__()
+
+	def __next__(self):
+		k=self._st_datas.__next__()
+		print k
+		return k
+
+	def __getitem__(self, index):
 		try:
-			return self._st_datas[str(date)]
+			return self._st_datas[str(index)]
 		except:
 			return None
-		#for k in self._st_datas:
-		#	if(k.get_date()==date):
-		#		return k
-		#return None
 
-	def get_score(self):
-		return self._score
 
-	def _set_score(self,s):
-		self._score=s
-
-	def __cmp__(self,obj):
-		return cmp(self.get_score(),obj.get_score())
-
-	def _do_analysis(self):
-		raise Exception("_do_analysis shall be override on lowe class")
-
-	def _analyze(self):
+	def fill_data(self):
 		#fill
 		#self._st_datas
 		msft = yf.Ticker(self._idx)
@@ -103,16 +107,62 @@ class StockDataAnalysys(object):
 		per=str(tot_time.days)+"d"
 		#print per
 		# get historical market data
-		hist = msft.history(period=per)
+		try:
+			hist = msft.history(period=per)
+		except:
+			time.sleep(4)
+			hist = msft.history(period=per)
 
 		for i in range(0,len(hist['Open'])):
 			dt=datetime.strptime(str(hist.axes[0][i]), "%Y-%m-%d %H:%M:%S")
 			stock=StockDaykMeasure(self._idx,dt,hist['Open'][i],hist['Close'][i],(hist['Dividends'][i]!=0.0))
 			self._st_datas[str(stock.get_date())]=stock
 			# print stock
-		self._do_analysis()
+
+
+class StockDataAnalysys(object):
+	
+	def __init__(self):
+		self._score=0
+		self._hist=None
+
+	def __init__(self,date_start,date_end,idx):
+		self._score=0
+		self._hist=StockHistoricMeasure(date_start,date_end,idx)
+
+	def __str__(self):
+		return "%s:%f"%(str(self._hist),self.get_score())
+
+	def get_score(self):
+		return self._score
+
+	def _set_score(self,s):
+		self._score=s
+
+	def _get_hist(self):
+		return self._hist
+
+	def __cmp__(self,obj):
+		return cmp(self.get_score(),obj.get_score())
+		
+	
+	def analyze(self):
+		raise Exception("analyze shall be override on lowe class")
 
 class DividendAnalysis(StockDataAnalysys):
+
+	def __init__(self):
+		self._div_dates=[]
+		self._dividend_period=0
+		self._days_buy=[]
+		self._days_sell=[]
+		self._benefits=[]
+		self._date_buy=None
+		self._date_sell=None
+		self._benefit_mean=0
+		self._std_dev_day_buy=0
+		self._std_dev_day_sell=0
+		super(DividendAnalysis,self).__init__()
 
 	def __init__(self,date_start,date_end,idx):
 		self._div_dates=[]
@@ -137,19 +187,20 @@ class DividendAnalysis(StockDataAnalysys):
 		return ret
 
 	def _calc_dividend_dates(self):
-		for k in self._st_datas:
-			if(self._st_datas[k].is_dividend()):
-				self._div_dates.append(self._st_datas[k].get_date())
+		hist=self._get_hist()
+		for k in hist:
+			if(hist[k].is_dividend()):
+				self._div_dates.append(hist[k].get_date())
 
 	def _get_next_dividend_date(self):
 		if(self._dividend_period==0):
 			return None
 
 		td=timedelta(days=self._dividend_period)
-		return(self._div_dates[len(self._div_dates)-1]+td)		
+		return(self._div_dates[len(self._div_dates)-1]+td)
 
 	def _calc_dividend_period(self):
-		
+
 		if (len(self._div_dates)<2):
 			self._dividend_period=0
 		else:
@@ -160,12 +211,13 @@ class DividendAnalysis(StockDataAnalysys):
 	def _calc_segment(self,date_start,date_div,date_end):
 		curr_d=date_start
 
+		hist=self._get_hist()
 		delta_t=timedelta(days=1)
 		min=9999999
 		min_date=date_start
-		min_st=self._get_stock_measure(curr_d)
+		min_st=hist[curr_d]
 		while(curr_d<date_div):
-			st=self._get_stock_measure(curr_d)
+			st=hist[curr_d]
 			if (st!=None) and (st.get_close()<min):
 				min=st.get_close()
 				min_date=curr_d
@@ -175,9 +227,9 @@ class DividendAnalysis(StockDataAnalysys):
 		curr_d=min_date
 		max=0
 		max_date=min_date
-		max_st=self._get_stock_measure(curr_d)
+		max_st=hist[curr_d]
 		while(curr_d<date_end):
-			st=self._get_stock_measure(curr_d)
+			st=hist[curr_d]
 			if (st!=None) and (st.get_close()>max):
 				max_date=curr_d
 				max_st=st
@@ -206,7 +258,7 @@ class DividendAnalysis(StockDataAnalysys):
 		self._benefits.append(benefit)
 
 
-	def _do_analysis(self):
+	def analyze(self):
 		self._calc_dividend_dates()
 		self._calc_dividend_period()
 		if(self._dividend_period==0):
@@ -219,13 +271,13 @@ class DividendAnalysis(StockDataAnalysys):
 		day_buy_mean=mean(self._days_buy)
 		day_sell_mean=mean(self._days_sell)
 		self._benefit_mean=mean(self._benefits)
-		
+
 		#print "buy mean:",day_buy_mean
 		#print "sell mean:",day_sell_mean
 
 		self._std_dev_day_buy=st_dev(self._days_buy,day_buy_mean)
 		self._std_dev_day_sell=st_dev(self._days_sell,day_sell_mean)
-		
+
 
 		next_dividend=self._get_next_dividend_date()
 		td_buy=timedelta(days=day_buy_mean)
@@ -238,6 +290,13 @@ class DividendAnalysis(StockDataAnalysys):
 		self._set_score(score)
 
 
+def analizer(stock_analisys):
+	stock_analisys.analyze()
+	return stock_analisys
+
+def data_filler(stock_historic_meas):
+	pass
+		
 if __name__ == "__main__":
 
 	parser = argparse.ArgumentParser()
@@ -245,7 +304,7 @@ if __name__ == "__main__":
 	parser.add_argument('--all_nasdaq',required=False,nargs='?',type=int,const=1,metavar='<all_nasdaq>',dest='all_nasdaq')
 	parser.add_argument('-hist',required=True,metavar='<history_days>',type=int,dest='hist_days')
 	configuration = parser.parse_args()
-	
+
 	if(configuration.all_nasdaq):
 		idx_list=all_index_nasdaq
 	elif(configuration.index_file_list):
@@ -254,6 +313,7 @@ if __name__ == "__main__":
 		print "argument error no index list"
 		sys.exit(1)
 
+	start=time.time()
 	td=datetime.now()
 	analisys_list=[]
 	count=0
@@ -261,8 +321,20 @@ if __name__ == "__main__":
 		kk=DividendAnalysis(td-timedelta(days=configuration.hist_days),td,i)
 		analisys_list.append(kk)
 		count+=1
-		sys.stdout.write("\r%03d%%"%(count*100/len(idx_list)))
+		sys.stdout.write("\r%02d%%"%(count*100/len(idx_list)))
 		sys.stdout.flush()
+
+	p = Pool(2)
+	analisys_list=p.map(analizer, analisys_list)
+	p.close()
+	p.join()
+
+	# count=0
+	# for a in analisys_list:
+	# 	a.analyze()
+	# 	count+=1
+	# 	sys.stdout.write("\r%02d%%"%(count*100/len(analisys_list)))
+	# 	sys.stdout.flush()
 
 	analisys_list.sort()
 	count=0
@@ -271,3 +343,5 @@ if __name__ == "__main__":
 		count+=1
 		if(count>5):
 			break
+	end=time.time()
+	print "elapsed time %f"%(end-start)
